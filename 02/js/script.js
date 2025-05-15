bc2292624ba0b269e758ac67bcdf49bcc07544d4
AranCygnus <106527337+AranCygnus@users.noreply.github.com>
2025-05-16 00:53:39 +0800

    upload 0516

---
+++ 02/js/script.js
@@ -0,0 +1,9 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings: ["Full Stack Developer", "Web Developer"],
+    typeSpeed: 100,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 60,
+    loop: true,
+});
